1f3931bde731f9e5e19cc1280f900fa386cf2f96
albert <gaotiecheng1982@gmail.com>
2017-02-09 17:02:11 +0800

    add

--- dataStructure/sortTest.py
+++ dataStructure/sortTest.py
@@ -1,9 +1,24 @@
 # 循环格式
 from dataStructure.bubbleSort import bubblesort
+from dataStructure.quickSort import quicksort
 
-A = [12, 35, 99, 16, 76]
-bubblesort(A)
-print(A)
+import string
 
-for i in range(0,6,1):
-    print(i)
+#list = map(int, sys.argv[1:])  # Get all the arguments
+
+list = [67, 78, 12, 35, 99, 16, 76]
+
+#-------bubblesort -------------
+#bubblesort(list)
+#print(list)
+
+# for i in range(0,6,1):
+#     print(i)
+
+#----------quickSort -----------
+start = 0
+end = len(list) - 1
+quicksort(list, start, end)  # Sort the entire list of arguments
+
+
+print(list)
