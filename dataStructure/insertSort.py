18404375eb446a02a1af08d2c89f931c088f6406
albert <gaotiecheng1982@gmail.com>
2017-02-24 17:28:35 +0800

    add

---
+++ dataStructure/insertSort.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def insertSort(lst):
+    if len(lst) == 1:
+        return lst
+
+    for i in range(1, len(lst)):
+        temp = lst[i]
+        j = i - 1
+        while j >= 0 and temp < lst[j]:
+            lst[j + 1] = lst[j]
+            j -= 1
+        lst[j + 1] = temp
+
+    return lst
--- dataStructure/sortTest.py
+++ dataStructure/sortTest.py
@@ -1,8 +1,9 @@
 # 循环格式
+
+from dataStructure.insertSort import insertSort
 from dataStructure.bubbleSort import bubblesort
 from dataStructure.quickSort import quicksort
 
-import string
 
 #list = map(int, sys.argv[1:])  # Get all the arguments
 
@@ -16,9 +17,12 @@ list = [67, 78, 12, 35, 99, 16, 76]
 #     print(i)
 
 #----------quickSort -----------
-start = 0
-end = len(list) - 1
-quicksort(list, start, end)  # Sort the entire list of arguments
+# start = 0
+# end = len(list) - 1
+# quicksort(list, start, end)  # Sort the entire list of arguments
 
+
+#============insertSort.py=================
+insertSort(list)
 
 print(list)
