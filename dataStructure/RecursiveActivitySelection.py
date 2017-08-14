b243f961d3710251f182c13bf8cd2ffff9356784
albert <gaotiecheng1982@gmail.com>
2017-08-14 18:50:56 +0800

    add

--- dataStructure/RecursiveActivitySelection.py
+++ dataStructure/RecursiveActivitySelection.py
@@ -9,8 +9,8 @@ single person, one at a time"""
 # s[]--> An array that contains start time of all activities
 # f[] --> An array that conatins finish time of all activities
 
-def printMaxActivities(s, f):
-    n = len(f)
+def printMaxActivities(stTime, fnsTime):
+    atvCount = len(fnsTime)
     print("The following activities are selected")
 
     # The first activity is always selected
@@ -18,19 +18,35 @@ def printMaxActivities(s, f):
     print(i)
 
     # Consider rest of the activities
-    for j in range(n):
+    for j in range(atvCount):
 
         # If this activity has start time greater than
         # or equal to the finish time of previously
         # selected activity, then select it
-        if s[j] >= f[i]:
+        if stTime[j] >= fnsTime[i]:
             print(j)
-            i=j
+            i = j
+
+
+def recursive_activity_selector(started, finished, result, i, j):
+    if i >= j:
+        return
+    l = i + 1
+    while l <= j:
+        if started[l] >= finished[i] and finished[l] <= started[j]:
+            result.append(l)
+            break
+        l += 1
+    recursive_activity_selector(started, finished, result, l, j);
 
 
 if __name__ == '__main__':
-
     # Driver program to test above function
     s = [1, 3, 0, 5, 8, 5]
     f = [2, 4, 6, 7, 9, 9]
     printMaxActivities(s, f)
+
+    result = []
+    n = len(s) - 1
+    i = 0
+    recursive_activity_selector(s, f, result, i, n)
